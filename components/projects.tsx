c7b2839d8e673dfe924e1a1c6883b75948f0627c
obrad24 <obrad10pejic@gmail.com>
2024-01-29 23:43:51 +0100

    ponedeljak

--- components/projects.tsx
+++ components/projects.tsx
@@ -16,7 +16,6 @@ export default function Projects() {
                         <React.Fragment key={index}>
                             <Project {...project} />
                         </React.Fragment>
-
                     ))
                 }
             </div>
@@ -27,15 +26,18 @@ export default function Projects() {
 type ProjectProps = typeof projectsData[number]
 
 function Project({ title, description, tags, imageUrl }: Temp) {
-    return <section className='bg-gray-100 max-w-[42rem] border bordfer-black/5 overflow-hidden sm:pr-8 mx-auto'>
-        <h3 className='text-2xl font-semibold'>{title}</h3>
-        <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
-        <ul className='flex flex-wrap mt-4 gap-2'>
-            {tags.map((tag, index) => {
-               return  <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
-            })}
-        </ul>
-
-        <Image src={imageUrl} alt="Projects" quality={95} />
+    return <section className='group bg-gray-100 hover:bg-gray-200 transition max-w-[50rem] rounded-lg border bordfer-black/5 overflow-hidden sm:pr-8 mx-auto relative sm:h-[28rem] mb-3 sm:mb-8 last:mb-0'>
+        <div className='flex flex-col py-4 px-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 group-even:ml-[26rem]'>
+            <h3 className='text-2xl font-semibold'>{title}</h3>
+            <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
+            <ul className='flex flex-wrap mt-4 gap-2'>
+                {tags.map((tag, index) => {
+                    return <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
+                })}
+            </ul>
+            <Image className='absolute top-8 -right-20 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[iniital] group-even:-left-20 group-hover:-translate-x-3
+            group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] transition group-even:group-hover:translate-x-3
+            group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2' src={imageUrl} alt="Projects" quality={95} />
+        </div>
     </section>
 }
