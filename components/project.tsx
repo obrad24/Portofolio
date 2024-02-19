c87aa747f8b3cc84a53795552e869dd5d38f4884
obrad24 <obrad10pejic@gmail.com>
2024-02-19 22:53:14 +0100

    ponedeljak

---
+++ components/project.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Temp } from "@/app/types/temp"
+import { projectsData } from "@/lib/data"
+import { motion, useScroll, useTransform } from "framer-motion"
+import { useRef } from "react"
+import Image from "next/image"
+
+
+type ProjectProps = typeof projectsData[number]
+
+export default function Project({ title, description, tags, imageUrl }: Temp) {
+    const ref = useRef<HTMLDivElement>(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["0 1", "1.33 1"],
+    });
+    const scaleProgress = useTransform(scrollYProgress, [0,1],[0.7,1]);
+    const opacityProgres = useTransform(scrollYProgress, [0,1],[0.6,1])
+
+    return <motion.div
+        ref={ref}
+        style={{
+            scale: scaleProgress,
+            opacity: opacityProgres,
+        }}
+        className="group mb-3 sm:mb-8 last:mb-0">
+        <section
+            className='bg-gray-100 hover:bg-gray-200 transition max-w-[50rem] rounded-lg border border-black/5 overflow-hidden sm:pr-8 mx-auto relative sm:h-[28rem]'>
+            <div className='flex flex-col py-4 px-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 group-even:ml-[26rem] grouo-even:pl-8'>
+                <h3 className='text-2xl font-semibold'>{title}</h3>
+                <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
+                <ul className='flex flex-wrap mt-4 gap-2'>
+                    {tags.map((tag, index) => {
+                        return <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
+                    })}
+                </ul>
+                <Image className='absolute top-8 -right-20 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[iniital] group-even:-left-20 group-hover:-translate-x-3
+            group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] transition group-even:group-hover:translate-x-3
+            group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2' src={imageUrl} alt="Projects" quality={95} />
+            </div>
+        </section>
+    </motion.div>
+}
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import SectionHeading from './section-heading';
 import { projectsData } from '@/lib/data';
-import Image from 'next/image';
-import { Temp } from '@/app/types/temp';
+import Project from './project';
 
 export default function Projects() {
     return (
@@ -23,21 +22,4 @@ export default function Projects() {
     )
 }
 
-type ProjectProps = typeof projectsData[number]
 
-function Project({ title, description, tags, imageUrl }: Temp) {
-    return <section className='group bg-gray-100 hover:bg-gray-200 transition max-w-[50rem] rounded-lg border bordfer-black/5 overflow-hidden sm:pr-8 mx-auto relative sm:h-[28rem] mb-3 sm:mb-8 last:mb-0'>
-        <div className='flex flex-col py-4 px-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:w-1/2 group-even:ml-[26rem]'>
-            <h3 className='text-2xl font-semibold'>{title}</h3>
-            <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
-            <ul className='flex flex-wrap mt-4 gap-2'>
-                {tags.map((tag, index) => {
-                    return <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
-                })}
-            </ul>
-            <Image className='absolute top-8 -right-20 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[iniital] group-even:-left-20 group-hover:-translate-x-3
-            group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] transition group-even:group-hover:translate-x-3
-            group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2' src={imageUrl} alt="Projects" quality={95} />
-        </div>
-    </section>
-}
